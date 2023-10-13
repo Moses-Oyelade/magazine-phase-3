85eccf7004025786f5f53497e5c6e362950a0334
Moses-Oyelade <oyelademoses@gmail.com>
2023-10-13 14:12:06 +0100

    fine_by_name

--- lib/Magazine.py
+++ lib/Magazine.py
@@ -5,7 +5,7 @@
 
 class Magazine:
     
-    all = []
+    ALL = []
     
     def __init__(self, name="", category=""):
         self.name=name
@@ -26,21 +26,33 @@ class Magazine:
     
     @classmethod
     def magazine_all(cls, magazine):
-        cls.all.append(magazine)
+        cls.ALL.append(magazine)
         
     @classmethod
     def show_all_magazines(cls):
-        print([{magazine.name: magazine.category}  for magazine in cls.all ])
+        print([{magazine.name: magazine.category}  for magazine in cls.ALL])
         
     def contributors(self):
         return self.name 
+    
+    @classmethod
+    def find_by_name(cls, name):
+        for name in cls.ALL:
+            if cls.ALL.__contains__(name):
+                return name.__dict__
+            else:
+                return"Not yet pulished"
+    
+
+    
+    
+    
     
     
 magazine1 = Magazine("Mr. Bean", "Medical")
 magazine2 = Magazine("Mr. Sean", "Banking")
 magazine3 = Magazine("Mr. Hunter", "Research")
-
-
+magazineA = Magazine("John Bosko", "Technology")
 
 # magazine.name = "Colorado"
 # magazine.category = "Medical"
@@ -50,6 +62,8 @@ magazine3 = Magazine("Mr. Hunter", "Research")
 # magazine2.category = "Research"
 
 # print(magazine.get_name())
-# print(magazine.get_category())
+
 # print(magazine.magazine_all())
-Magazine.show_all_magazines()
+print(Magazine.show_all_magazines())
+
+print(Magazine.find_by_name("Mr. Sean"))
